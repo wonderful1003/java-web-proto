837425592f9499a27ec53cb7fedfa7227de891e9
Seunghyun Bang <wisecat1003@naver.com>
2025-10-13 13:44:38 +0900

    test

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String root() {
-        return "Hello from Spring Boot on Cloud Run!";
+        return "Hello from Spring Boot on Cloud Run! test";
     }
 }
